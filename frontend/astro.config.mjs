a10d7a1fc4519c9215f0f7f6304606a0f43483e5
Hamza Daher <hada@192.168.0.103>
2025-06-08 17:51:49 +0100

    added odyssey template

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,7 +1,14 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx";
+import icon from "astro-icon";
+import lit from "@astrojs/lit";
+import vue from '@astrojs/vue';
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'static',
+  site: 'https://odyssey-theme.sapling.supply/', // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+  sitemap: true, // Generate sitemap (set to "false" to disable)
+  integrations: [sitemap(), mdx(), lit(), icon(), vue()], // Add renderers to the config
+  output: 'static', // Output directory for the built site
 });
