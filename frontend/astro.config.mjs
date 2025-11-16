bbe39043dcac5a987f678a307246d299a6b72617
Hamza Daher <hada@MacBook-Air-de-Hamza.local>
2025-11-16 23:31:05 +0100

    fix build

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -3,14 +3,13 @@ import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
 import icon from 'astro-icon';
 import lit from '@astrojs/lit';
-import vue from '@astrojs/vue';
 import astroI18next from "astro-i18next";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://truemoroccotours.com', // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
   sitemap: true, // Generate sitemap (set to "false" to disable)
-  integrations: [sitemap(), mdx(), lit(), icon(), vue(), astroI18next()], // Add renderers to the config
+  integrations: [sitemap(), mdx(), lit(), icon(), astroI18next()], // Add renderers to the config
   output: 'static', // Output directory for the built site
   defaultLocale: "en",
   locales: ["en", "fr", "es"],
