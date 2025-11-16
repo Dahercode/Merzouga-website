b3419134cd184dafcd57f9de3f38546cec33bcd1
Hamza Daher <hada@MacBook-Air-de-Hamza.local>
2025-11-16 17:02:55 +0100

    added i18 astro next

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -4,11 +4,14 @@ import mdx from '@astrojs/mdx';
 import icon from 'astro-icon';
 import lit from '@astrojs/lit';
 import vue from '@astrojs/vue';
+import astroI18next from "astro-i18next";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://truemoroccotours.com', // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
   sitemap: true, // Generate sitemap (set to "false" to disable)
-  integrations: [sitemap(), mdx(), lit(), icon(), vue()], // Add renderers to the config
+  integrations: [sitemap(), mdx(), lit(), icon(), vue(), astroI18next()], // Add renderers to the config
   output: 'static', // Output directory for the built site
+  defaultLocale: "en",
+  locales: ["en", "fr", "es"],
 });
